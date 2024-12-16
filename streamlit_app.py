7acb2dca7fb1fdf1a5402351ea4bc575a9a562de
Kamil Raczycki <raczyckikamil@gmail.com>
2024-12-16 11:08:20 +0000

    feat: start creating the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
+import time
 import streamlit as st
+import plotly.express as px
 
-st.title("🎈 My new app")
+st.title("City Summit 🏙️🗻")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Generate a summit for the city of your choosing!"
 )
+
+city_name = st.text_input('City')
+resolution = st.slider("Resolution", min_value=1,
+                       max_value=10, value=1, step=1)
+rotate_buildings = st.checkbox("Rotate buildings", value=True)
+
+if st.button('Generate!'):
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+    st.success("Done!")
+
+    df = px.data.iris()
+    fig = px.scatter(
+        df,
+        x="sepal_width",
+        y="sepal_length",
+        color="species",
+        size="petal_length",
+        hover_data=["petal_width"],
+    )
+
+    event = st.plotly_chart(fig, key="iris", on_select="rerun")
