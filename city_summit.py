97e3e4ea785cfbe0c56933620c505c415e81f3df
Kamil Raczycki <raczyckikamil@gmail.com>
2024-12-17 00:04:28 +0000

    chore: change caching logic

--- city_summit.py
+++ city_summit.py
@@ -11,7 +11,7 @@ import pyproj as proj
 import streamlit as st
 from affine import Affine
 from matplotlib.colors import ListedColormap
-from overturemaestro import convert_geometry_to_geodataframe, geocode_to_geometry
+from overturemaestro import convert_geometry_to_geodataframe, convert_geometry_to_parquet, geocode_to_geometry
 from pypalettes import load_cmap
 from rasterio.features import MergeAlg, rasterize
 from rich.progress import track
@@ -65,11 +65,29 @@ def get_aligned_geometry(geometry: BaseGeometry, skip_rotation: bool = False, sk
 
     raise RuntimeError("Rotation not found")
 
+@st.cache_data(show_spinner=False, persist="disk", ttl=24 * 3600)
+def download_overturemaps_data(location: str) -> gpd.GeoDataFrame:
+    buildings_path = convert_geometry_to_parquet(
+        "buildings",
+        "building",
+        geocode_to_geometry(location),
+        # columns_to_download=["id", "geometry"],
+    )
+    buildings = convert_geometry_to_geodataframe(
+        "buildings",
+        "building",
+        geocode_to_geometry(location),
+        # columns_to_download=["id", "geometry"],
+    )
+    buildings_path.unlink()
+    return buildings
 
+
+@st.cache_data(show_spinner=False, persist="disk", ttl=24 * 3600)
 def get_aligned_buildings(
-    st_container: DeltaGenerator, location: str, skip_rotation: bool = False, sample_size: Optional[int] = None, use_utm_projection: bool = True
-) -> list[BaseGeometry]:
-    with st_container:
+    _st_container: DeltaGenerator, location: str, skip_rotation: bool = False, sample_size: Optional[int] = None, use_utm_projection: bool = True
+) -> gpd.GeoDataFrame:
+    with _st_container:
         with st.spinner('Downloading buildings from Overture Maps'):
             buildings = convert_geometry_to_geodataframe(
                 "buildings",
@@ -77,10 +95,11 @@ def get_aligned_buildings(
                 geocode_to_geometry(location),
                 # columns_to_download=["id", "geometry"],
             )
-
-    if use_utm_projection:
-        utm_crs = buildings.estimate_utm_crs()
-        buildings = buildings.to_crs(utm_crs)
+        
+        if use_utm_projection:
+            with st.spinner('Projecting buildings to UTM CRS'):
+                utm_crs = buildings.estimate_utm_crs()
+                buildings = buildings.to_crs(utm_crs)
 
     if not sample_size:
         sample_size = len(buildings)
@@ -89,7 +108,7 @@ def get_aligned_buildings(
     with ProcessPoolExecutor() as ex:
         fn = partial(get_aligned_geometry,
                      skip_projection=use_utm_projection, skip_rotation=skip_rotation)
-        with st_container:
+        with _st_container:
             current_progress = 0.0
             step_size = 1 / sample_size
             bar = st.progress(value=current_progress,
@@ -142,23 +161,27 @@ def generate_plotly_figure(
 
 
 def get_city_summit(st_container: DeltaGenerator, city: str, resolution: int, skip_rotation: bool, palette_name: str) -> go.Figure:
-    if skip_rotation:
-        loaded_geometries_path = Path(
-            f"files/buildings/{city.lower()}/aligned.parquet")
-    else:
-        loaded_geometries_path = Path(
-            f"files/buildings/{city.lower()}/rotated.parquet")
-
-    if not loaded_geometries_path.exists():
-        loaded_geometries = get_aligned_buildings(
-            st_container=st_container,
+    loaded_geometries = get_aligned_buildings(
+            _st_container=st_container,
             location=city, skip_rotation=skip_rotation)
-        loaded_geometries_path.parent.mkdir(exist_ok=True, parents=True)
-        loaded_geometries.to_parquet(loaded_geometries_path)
-    else:
-        with st_container:
-            with st.spinner('Reading cached buildings'):
-                loaded_geometries = gpd.read_parquet(loaded_geometries_path)
+
+    # if skip_rotation:
+    #     loaded_geometries_path = Path(
+    #         f"files/buildings/{city.lower()}/aligned.parquet")
+    # else:
+    #     loaded_geometries_path = Path(
+    #         f"files/buildings/{city.lower()}/rotated.parquet")
+
+    # if not loaded_geometries_path.exists():
+    #     loaded_geometries = get_aligned_buildings(
+    #         st_container=st_container,
+    #         location=city, skip_rotation=skip_rotation)
+    #     loaded_geometries_path.parent.mkdir(exist_ok=True, parents=True)
+    #     loaded_geometries.to_parquet(loaded_geometries_path)
+    # else:
+    #     with st_container:
+    #         with st.spinner('Reading cached buildings'):
+    #             loaded_geometries = gpd.read_parquet(loaded_geometries_path)
 
     gs = loaded_geometries.geometry
     minx, miny, maxx, maxy = gs.total_bounds
